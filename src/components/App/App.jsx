55cc0d897c6eb0a2065a67c364c8fe30cc818ed1
Myles Voigt <myles.voigt@gmail.com>
2023-02-04 17:08:39 -0600

    setup redux

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import axios from 'axios';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import FeelingPart from '../FeelingPart/FeelingPart.jsx';
+import { useHistory } from 'react-router-dom';
 
 function App() {
+  const history = useHistory();
 
   return (
     <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Feedback!</h1>
-        <h4>Don't forget it!</h4>
-      </header>
+      <Router>
+        <header className='App-header'>
+          <h1 className='App-title'>Feedback!</h1>
+          <h4>Don't forget it!</h4>
+        </header>
+        
+        <Route exact path='/feeling'>
+          <FeelingPart />
+        </Route>
+        <button onClick={() => history.push('/feeling')}>Add New Feedback</button>
+      </Router>
     </div>
   );
 }
---
+++ src/components/FeelingPart/FeelingPart.jsx
@@ -0,0 +1,10 @@
+function Feeling() {
+    return(
+        <>
+        <h3>How are you feeling today?</h3>
+        <input required></input>
+        </>
+    );
+}
+
+export default Feeling;
