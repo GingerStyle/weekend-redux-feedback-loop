d8123bd930d13888c32d4d204ca49fcf72891834
Myles Voigt <myles.voigt@gmail.com>
2023-02-07 13:58:34 -0600

    added ; to end of sql statement

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -7,7 +7,7 @@ const pool = require('../modules/pool.js');
 router.post('/', (req, res) => {
     console.log('in router.post');
     let newFeedback = req.body;
-    let queryText = `INSERT INTO "feedback" ("feeling", "understanding", "support", "comments") VALUES ($1, $2, $3, $4)`;
+    let queryText = `INSERT INTO "feedback" ("feeling", "understanding", "support", "comments") VALUES ($1, $2, $3, $4);`;
     pool.query(queryText, [newFeedback.feelings, newFeedback.understand, newFeedback.supportLevel, newFeedback.comment])
     .then(() => {
         res.sendStatus(201);
